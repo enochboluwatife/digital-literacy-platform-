25eb945562266cc20e92e2dc75e67e5ee696faa4
Enoch Boluwatife <omogbohunperez@gmail.com>
2025-09-14 02:50:49 +0100

    Fix python-jose import and update requirements

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from typing import Optional, Dict, Any, Union
-from python_jose import JWTError, jwt
+from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
